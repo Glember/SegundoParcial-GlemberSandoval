8217c72636587fcc564e2d5000832191653e04d9
Glember <sandovalglember40@gmail.com>
2021-09-18 23:13:09 -0600

    Codigo subido

---
+++ public/js/api.js
@@ -0,0 +1,13 @@
+let api_key = "cc79830b396b9b71a6fc22081afaef6e";
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
+let movie_años_url = "/discover/movie?primary_release_year?";
+let tv_popular_url = "https://api.themoviedb.org/3/tv/popular?api_key=cc79830b396b9b71a6fc22081afaef6e&language=es";
+let pelicula_año_url = "https://api.themoviedb.org/3/discover/movie?api_key=cc79830b396b9b71a6fc22081afaef6e&language=es&year=";
+let peliculas_actuales_url = "https://api.themoviedb.org/3/movie/now_playing?api_key=cc79830b396b9b71a6fc22081afaef6e&language=es";
+let certificaciones = "https://api.themoviedb.org/3/certification/movie/list?api_key=cc79830b396b9b71a6fc22081afaef6e";
+let adultosR = "https://api.themoviedb.org/3/discover/movie?api_key=cc79830b396b9b71a6fc22081afaef6e&language=es&certification=R&include_adult=true";
